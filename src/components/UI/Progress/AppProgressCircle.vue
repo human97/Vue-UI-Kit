d6b85f3d75c3d4246f00ad7188aee947e314ba47
Alex <xinfoz19@gmail.com>
2023-10-02 23:48:44 +0200

    feat: Add UI progress circle component

---
+++ src/components/UI/Progress/AppProgressCircle.vue
@@ -0,0 +1,71 @@
+<script setup>
+const props = defineProps( {
+    progress: {
+        type: Number,
+        required: true, 
+    },
+    color: {
+        type: String,
+        default: 'primary',
+    },
+})
+</script>
+
+<template>
+    <div class="progress-circle">
+        <span 
+            class="progress-circle-percent"
+            :style="[{'color': `var(--${color})`}]"
+        >
+            {{ progress }}%
+        </span>
+        <svg width="120" height="120" viewBox="0 0 120 120" class="progress-circle-svg">
+          <circle
+            cx="60"
+            cy="60"
+            r="54"
+            fill="none"
+            :stroke="`var(--${color}-hover)`"
+            stroke-width="12" />
+          <circle
+            class="progress-circle-line"
+            :style="[{'stroke-dashoffset': `calc(100 - ${progress})`}]"
+            cx="60"
+            cy="60"
+            r="54"
+            fill="none"
+            :stroke="`var(--${color})`"
+            stroke-width="12"
+            pathLength="100" />
+        </svg>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.progress {
+  &-circle {
+    position: relative;
+    width: 120px;
+    height: 120px;
+    &-svg {
+      transform: rotate(-90deg);
+    }
+    &-percent {
+      position: absolute;
+      left: 0;
+      width: 100%;
+      top: 0;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: bold;
+      font-size: 18px;
+    }
+    &-line {
+      stroke-dasharray: 100;
+      transition: .5s;
+    }
+  }
+}
+</style>
--- src/views/ProgressView.vue
+++ src/views/ProgressView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AppProgressBar from '@/components/UI/Progress/AppProgressBar.vue'
+import AppProgressCircle from '@/components/UI/Progress/AppProgressCircle.vue'
 import AppButton from '@/components/UI/AppButton.vue'
 import {ref} from 'vue'
 
-const progressBar = ref(50)
+const progressBar = ref(40)
+const progressCircle = ref(60)
 
 const increaseProgressBar = () => {
     if (progressBar.value < 100) {
@@ -16,10 +18,23 @@ const decreaseProgressBar = () => {
         progressBar.value -= 10
     }
 }
+
+const increaseProgressCircle = () => {
+    if (progressCircle.value < 100) {
+        progressCircle.value += 10
+    }
+}
+
+const decreaseProgressCircle = () => {
+    if (progressCircle.value > 0) {
+        progressCircle.value -= 10
+    }
+}
 </script>
 
 <template>
     <h1 class="heading-1">Progress</h1>
+
     <h2 class="heading-2">Progress Bar</h2>
     <div class="line line_block">
         <app-progress-bar 
@@ -28,7 +43,7 @@ const decreaseProgressBar = () => {
             color="warning"
         />
 
-        <div class="line wrap-btn">
+        <div class="line wrap-btn-bar">
             <app-button
                 color="danger"
                 label="Minus 10%"
@@ -45,11 +60,45 @@ const decreaseProgressBar = () => {
             />
         </div>
     </div>
+
+    <h2 class="heading-2">Progress Circle</h2>
+    <div class="line wrap-circle-block m-r-type">
+        <app-progress-circle
+            :progress="progressCircle"
+            color="info"
+        />
+
+        <div class="wrap-btn-circle m-b-type">
+            <app-button
+                color="danger"
+                label="Minus 10%"
+                fixed="medium"
+                :disabled="progressCircle === 0"
+                @click="decreaseProgressCircle"
+            />
+            <app-button
+                color="success"
+                label="Plus 10%"
+                fixed="medium"
+                :disabled="progressCircle === 100"
+                @click="increaseProgressCircle"
+            />
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-.wrap-btn {
+.wrap-btn-bar {
     width: 500px;
     justify-content: space-between;
+}
+
+.wrap-btn-circle {
+    display: flex;
+    flex-direction: column;
+}
+
+.wrap-circle-block {
+    align-items: center;
 }
 </style>
